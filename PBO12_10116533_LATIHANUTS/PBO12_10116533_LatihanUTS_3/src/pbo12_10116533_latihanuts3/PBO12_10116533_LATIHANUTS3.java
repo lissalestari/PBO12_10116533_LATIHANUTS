618fb0c9a91273747d62344d2f253db1cab7d4d6
lissalestari <32484181+lissalestari@users.noreply.github.com>
2017-11-14 22:13:12 +0700

    Add files via upload

---
+++ PBO12_10116533_LATIHANUTS/PBO12_10116533_LatihanUTS_3/src/pbo12_10116533_latihanuts3/PBO12_10116533_LATIHANUTS3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo12_10116533_latihanuts3;
+
+/**
+ *
+ * @author Lisa Setia Lestari
+ */
+public class PBO12_10116533_LATIHANUTS3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ PBO12_10116533_LATIHANUTS/PBO12_10116533_LatihanUTS_4/src/pbo12_10116533_latihanuts4/ModelEmployee.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo12_10116533_latihanuts4;
+
+/**
+ *
+ * @author Lisa Setia Lestari
+ */
+public class ModelEmployee {
+    
+    public float salary = 40000;
+    
+}
